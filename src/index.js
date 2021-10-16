d6d8ae7e1963c6578d1b0f5955e6894a7620ed5a
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2021-10-16 10:07:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -350,6 +350,7 @@ const checkBalances = async () => {
 <b>Status</b>: ${!test ? `\u{1F51B} Robô operando.` : `\u{1F6D1} Modo simulação.`} 
 <b>Data inicial</b>: ${dataInicial}
 <b>Dias ativado</b>: ${days}
+<b>Depósito inicial</b>: ${amountInitial}
 <b>Valor em operação</b>: ${amount}
 <b>Saldo BRL:</b> ${BRL} 
 <b>Saldo BTC:</b> ${BTC} (R$ ${(priceBTC.last * BTC).toFixed(2)})
